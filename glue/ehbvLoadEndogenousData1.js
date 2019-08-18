9a4b44d9855d2b73c9a6311ba3d98068b0e92e5d
Rob Gifford <robjgiff@gmail.com>
2019-08-18 14:48:42 +0100

    Further development of build to capture sequence table information for EVEs and EVE genome consensus seqs

--- glue/ehbvLoadEndogenousData1.js
+++ glue/ehbvLoadEndogenousData1.js
@@ -13,3 +13,17 @@ _.each(seqIds, function(seqId) {
     });
 });
 
+// list the hepadnavirus sequences
+var listSeqResultRefs = glue.command(["list", "sequence", "-w", "source.name = 'fasta-ehbv-consensus'"]);
+// extract from the result a list of sequence IDs.
+var refSeqIds = glue.getTableColumn(listSeqResultRefs, "sequenceID");
+
+// for each sequence ID
+_.each(refSeqIds, function(seqId) {
+    // create an object in the custom table which uses the sequence ID as the row ID.
+    glue.command(["create", "custom-table-row", "endogenous_data", seqId]);
+    // associate the corresponding sequence with this object.
+    glue.inMode("sequence/fasta-ehbv-consensus/"+seqId, function() {
+        glue.command(["set", "link-target", "endogenous_data", "custom-table-row/endogenous_data/"+seqId]);
+    });
+});
